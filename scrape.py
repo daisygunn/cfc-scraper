561826909bc2d6f3a19a968ea6c93c01f3379322
daisygunn <82104778+daisygunn@users.noreply.github.com>
2023-03-13 16:49:55 +0000

    Write second iteration of find_external_sources and run_programme to reuse code blocks

--- scrape.py
+++ scrape.py
@@ -9,34 +9,45 @@ def scrape_html(html_page):
 
     # Using beautiful soup, pass in data from get request and use html parser to parse content
     content = BeautifulSoup(html_content.content, 'html.parser')
+
     # return soup variable to be used later on
     return content
 
-def find_external_sources(html_content):
-    """ Function to retrieve scripts in html page """
-    
-    scripts = html_content.findAll("script")
-    images = html_content.findAll("img")
-    svg = html_content.findAll("svg")
-    links = html_content.findAll("link")
-    # create data variable
-    data = { 'scripts': str(scripts),
-             'images': str(images),
-             'svg': str(svg),
-             'links': str(links)
-            }
 
-    external_sources = json.dumps(data, indent=4)
-    print(external_sources)
+def find_external_sources(html_content, element, search_item):
+    """ Function to retrieve sources in html page """
+    all_resources = html_content.find_all(element)
+    external_resource_list = []
+    for x in all_resources:
+        if search_item in x.attrs:
+            if 'https' in x[search_item]:
+                external_resource_list.append(x)
+    return external_resource_list
 
 
 def run_programme():
     # Set html link to be scraped and call GET request
     html_link = "https://www.cfcunderwriting.com"
+
+    external_sources = []
     # pass link to scrape function
     html_scraped_content = scrape_html(html_link)
     # pass scraped content to retrieve external sources
-    find_external_sources(html_scraped_content)
+    scripts = find_external_sources(html_scraped_content, "script", "src")
+    external_sources.append(scripts)
+    images = find_external_sources(html_scraped_content, "img", "src")
+    external_sources.append(images)
+    svgs = find_external_sources(html_scraped_content, "svg", "src")
+    external_sources.append(svgs)
+    links = find_external_sources(html_scraped_content, "link", "href")
+    external_sources.append(links)
+    # create data variable
+    data = {'external sources count': len(external_sources),
+            'external sources': str(external_sources)
+            }
+
+    external_sources_json = json.dumps(data, indent=4)
+    print(external_sources_json)
 
 
 run_programme()
