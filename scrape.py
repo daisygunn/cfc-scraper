330bfe610220250655b42f0399f8d04be825cd58
daisygunn <82104778+daisygunn@users.noreply.github.com>
2023-03-13 16:05:28 +0000

    Write first iteration of find external sources function

--- scrape.py
+++ scrape.py
@@ -1,16 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
-# Set html link to be scraped and call GET request
-html_link = "https://www.cfcunderwriting.com"
 
 def scrape_html(html_page):
     """ Function to scrape desired html page """
     html_content = requests.get(html_page)
 
     # Using beautiful soup, pass in data from get request and use html parser to parse content
-    soup = BeautifulSoup(html_content.content, 'html.parser')
-    print(soup)
+    content = BeautifulSoup(html_content.content, 'html.parser')
     # return soup variable to be used later on
-    return soup
+    return content
 
+def find_external_sources(html_content):
+    """ Function to retrieve scripts in html page """
+    
+    scripts = html_content.findAll("script")
+    images = html_content.findAll("img")
+    svg = html_content.findAll("svg")
+    links = html_content.findAll("link")
+    # create data variable
+    data = { 'scripts': str(scripts),
+             'images': str(images),
+             'svg': str(svg),
+             'links': str(links)
+            }
+
+    external_sources = json.dumps(data, indent=4)
+    print(external_sources)
+
+
+def run_programme():
+    # Set html link to be scraped and call GET request
+    html_link = "https://www.cfcunderwriting.com"
+    # pass link to scrape function
+    html_scraped_content = scrape_html(html_link)
+    # pass scraped content to retrieve external sources
+    find_external_sources(html_scraped_content)
+
+
+run_programme()
